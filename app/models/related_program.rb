85fe3f22acc70c182ea86fcdeee29036fb1ffcc7
Alberto Jose Larios Sanchez <betoche_1@hotmail.com>
2019-09-11 23:46:57 +0000

    name function added to RelatedProgram model for Rails Admin use

--- app/models/related_program.rb
+++ app/models/related_program.rb
@@ -1,4 +1,8 @@
 class RelatedProgram < ApplicationRecord
     belongs_to :parent, class_name: 'Program', optional: true
     belongs_to :child, class_name: 'Program', optional: true
+
+    def name
+        "#{self.parent.member} - #{self.child.member}"
+    end
 end
