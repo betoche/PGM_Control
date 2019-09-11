2171749ab9e6148b7a16be80d48fbd41e9001bea
Alberto Jose Larios Sanchez <betoche_1@hotmail.com>
2019-09-11 23:19:50 +0000

    Finished working parents-children self model relationship

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,2 +1,6 @@
 class Code < ApplicationRecord
+
+    def name
+        "#{self.code}"
+    end
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -3,9 +3,9 @@ class Program < ApplicationRecord
   belongs_to :source_file
   belongs_to :code, optional: true
 
-  has_many :parent_programs, class_name: 'RelatedProgram', foreign_key: :parent_id
+  has_many :parent_programs, class_name: 'RelatedProgram', foreign_key: :child_id
   has_many :parents, through: :parent_programs, source: :parent
-  has_many :child_programs, class_name: 'RelatedProgram', foreign_key: :child_id
+  has_many :child_programs, class_name: 'RelatedProgram', foreign_key: :parent_id
   has_many :children, through: :child_programs, source: :child
 
   def name
