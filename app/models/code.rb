672d4480d20a48d3016c4ae4ed327a59cf06beb2
Alberto Jose Larios Sanchez <betoche_1@hotmail.com>
2019-09-19 22:05:37 +0000

    Error message box stylized, field validation added to code model

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,6 +1,7 @@
 class Code < ApplicationRecord
+  validates :code, :operation, :description, :home_dir, presence: true, allow_blank: false
 
-    def name
-        "#{self.code}"
-    end
+  def name
+    "#{self.code}"
+  end
 end
